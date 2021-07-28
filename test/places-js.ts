ddeb2fb7e8e25ee7679cfb785777ed3a9e115916
Joxit <joxit972@gmail.com>
2021-07-28 11:21:44 +0200

    fix: update maplibre and mapbix types versions and add missing options

--- test/places-js.ts
+++ test/places-js.ts
@@ -1,9 +1,9 @@
 import { Map as MapboxGLMap } from 'mapbox-gl';
 import { Map as MapLibreGLMap } from 'maplibre-gl';
+import L = require('leaflet');
 import { JawgPlaces } from '../';
 
-
-new JawgPlaces.Input({input: '', accessToken: '<Access-Token>'})
-new JawgPlaces.MapLibre({input: '', accessToken: '<Access-Token>'})
-new JawgPlaces.Mapbox({input: '', accessToken: '<Access-Token>'}).attachMap(new MapboxGLMap())
-new JawgPlaces.Leaflet({input: '', accessToken: '<Access-Token>', L: {}})
+new JawgPlaces.Input({ input: '', accessToken: '<Access-Token>' });
+new JawgPlaces.MapLibre({ input: '', accessToken: '<Access-Token>' }).attachMap(new MapLibreGLMap());
+new JawgPlaces.Mapbox({ input: '', accessToken: '<Access-Token>' }).attachMap(new MapboxGLMap());
+new JawgPlaces.Leaflet({ input: '', accessToken: '<Access-Token>', L }).attachMap(new L.Map('my-map'));
